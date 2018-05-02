d1ec8ebdc04e539404693343295785d69f75abe8
Igor Kozyrenko <igor@unity3d.com>
2018-05-02 19:08:26 +0300

    Update setup.py to include license and classifiers

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ setup(
     name='undname',
     version='0.1',
     packages=['undname'],
-    url='',
-    license='',
+    url='https://github.com/ikseek/undname',
+    license='LGPLv2.1',
     author='Igor Kozyrenko',
     author_email='igor@ikseek.com',
     description='CFFI wrapper for wine undname module',
+    long_description=open('README.rst').read(),
+    keywords=['undname', 'msvc'],
     setup_requires=["cffi"],
     install_requires=["cffi"],
-    cffi_modules=["undname/undname_build.py:ffi_builder"]
+    cffi_modules=["undname/undname_build.py:ffi_builder"],
+    classifiers=["Development Status :: 4 - Beta",
+                 "Topic :: Software Development :: Debuggers",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 "License :: OSI Approved"
+                 " :: GNU Lesser General Public License v2 or later (LGPLv2+)"]
 )
